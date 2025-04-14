6bf4a815c03f029ee68051ac2e881cc3db129459
plumjs1916 <j.slizewska.554@studms.ug.edu.pl>
2025-04-14 15:32:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            public static void main(String[] args) {
+                System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 10; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                String[] names = {"Piotr", "Anna", "Jan", "Leszek"};
+
+                for (String name : names) {
+                    System.out.println("Imię: " + name);
+                }
+            }
         }
-    }
-}
